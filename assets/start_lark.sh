32a7b29103c46fb80c65f4287209f74c2b1f0c3d
Barna Keresztes <barna.keresztes@ims-bordeaux.fr>
2026-08-03 11:24:24 +0200

    start script updated

--- assets/start_lark.sh
+++ assets/start_lark.sh
@@ -10,4 +10,10 @@ sleep 1
 cd /mnt/us/LARK
 LD_LIBRARY_PATH=$LIBDIR ./$LARKEXEC
 
-pkill -x $LARKEXEC
+# If the app didn't exit cleanly, it may have left system settings changed
+# (screensaver inhibited, BT keepalive enabled). Reset them before killing it.
+if pgrep -x $LARKEXEC > /dev/null; then
+    lipc-set-prop -s com.lab126.powerd preventScreenSaver 0
+    lipc-set-prop -s com.lab126.btfd ensureBTconnection 0
+    pkill -x $LARKEXEC
+fi
